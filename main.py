b4dc58fb6e8790c2cc5a1f807e628143e34fbea0
Norbu <ntsering91@gmail.com>
2015-09-02 14:40:21 -0400

    added a .gitignore and removed a comment

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python
-"""
-Things todo:
-    Add functionality to menu buttons
-
-    clean up the map's XML
-    
-    More Documentation
-"""
-
 import pygame
 import random
 import os
@@ -20,13 +11,13 @@ class Game(object):
 
     def __init__(self):
         super(Game, self).__init__()
-        
+
         pygame.init()
         self.surf_main = pygame.display.set_mode(WIN_SIZE)
         pygame.display.set_caption(CAPTION)
         self.debug = False
         self.running = True
-        
+
         self.scenes = {"scene1": Scene()}
         self.curr_scene = self.scenes["scene1"]
 
@@ -41,19 +32,19 @@ class Game(object):
 
     def on_event(self):
         self.events = pygame.event.get()
-        
+
         for e in self.events:
             if e.type == pygame.QUIT:
                 self.running = False
             elif e.type == pygame.KEYDOWN:
                 if e.key == pygame.K_ESCAPE:
                     self.running = False
-        
+
         self.curr_scene.on_event(self.events)
-    
+
     def on_update(self):
         self.curr_scene.on_update()
-    
+
     def on_render(self):
         self.curr_scene.on_render()
 
@@ -63,10 +54,10 @@ class Game(object):
             self.on_event()
             self.on_update()
             self.on_render()
-            
+
             pygame.display.update()
             self.surf_main.fill((80,80,100, 255))
-            
+
 
 
 
